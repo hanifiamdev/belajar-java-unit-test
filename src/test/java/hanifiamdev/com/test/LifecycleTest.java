2c52418fe86f8198894a08710e83ebea289a76f0
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-28 06:32:06 +0700

    Urutan Eksekusi Test

---
+++ src/test/java/hanifiamdev/com/test/LifecycleTest.java
@@ -0,0 +1,14 @@
+package hanifiamdev.com.test;
+
+public class LifecycleTest {
+
+    private String temp;
+
+    void  testA() {
+        temp = "Hanif";
+    }
+
+    void testB() {
+        System.out.println(temp);
+    }
+}
