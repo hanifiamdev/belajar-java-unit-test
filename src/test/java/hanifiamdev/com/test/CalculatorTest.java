ba397b6cc9daa3786d362a7012c3877718aea086
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-27 13:45:52 +0700

    Mengubah Nama Test

--- src/test/java/hanifiamdev/com/test/CalculatorTest.java
+++ src/test/java/hanifiamdev/com/test/CalculatorTest.java
@@ -1,13 +1,19 @@
 package hanifiamdev.com.test;
 
+import hanifiamdev.com.test.generator.SimpleDisplayNameGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+//@DisplayName("Test untuk Calculator class")
+@DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("Test skenario sukses untuk method add(integer, integer)")
     public void testAddSuccess() {
         var result = calculator.add(10, 10);
 
