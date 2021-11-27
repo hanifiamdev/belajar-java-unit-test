90022339ae183b59ce7f8f68b68e57f4ee74d624
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-27 14:31:43 +0700

    Sebelum dan Setelah Test

--- src/test/java/hanifiamdev/com/test/CalculatorTest.java
+++ src/test/java/hanifiamdev/com/test/CalculatorTest.java
@@ -1,10 +1,8 @@
 package hanifiamdev.com.test;
 
 import hanifiamdev.com.test.generator.SimpleDisplayNameGenerator;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 //@DisplayName("Test untuk Calculator class")
@@ -12,6 +10,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After all");
+    }
+
+    @BeforeEach
+    public void setUp(){
+        System.out.println("Before each");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("After each");
+
+    }
 
     @Test
     @DisplayName("Test skenario sukses untuk method add(integer, integer)")
